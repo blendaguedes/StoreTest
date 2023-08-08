7055206304610cb30ee881b94785cbefa20f8c6f
blendaguedes <blendaguedes@BLENDAWINDOWS>
2023-08-08 16:45:01 -0300

    Add project files.

---
+++ StoreTest/Models/Store.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StoreTest.Models
+{
+    public class Store
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Item")]
+        public string Item { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
